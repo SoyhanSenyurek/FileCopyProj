bf5e5fb078311615378a42f9cee578e707ab683b
tunahancetinkaya <tunahancetinkaya@marun.edu.tr>
2022-12-01 17:19:31 +0300

    Tunahan

--- FileCopyProj/MainWindow.xaml.cs
+++ FileCopyProj/MainWindow.xaml.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using MessageBox = System.Windows.Forms.MessageBox;
+using WebBrowser = System.Windows.Controls.WebBrowser;
 
 namespace FileCopyProj
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -38,6 +45,8 @@ namespace FileCopyProj
                 if (result == System.Windows.Forms.DialogResult.OK)
                 {
                     SourceFileNameTextBox.Text = dialog.SelectedPath;
+                    SourceFolderView.Navigate(new Uri(SourceFileNameTextBox.Text));
+
                 }
             }
         }
@@ -51,15 +60,17 @@ namespace FileCopyProj
                 if (result == System.Windows.Forms.DialogResult.OK)
                 {
                     TargetFileNameTextBox.Text = dialog.SelectedPath;
+                    TargetFolderView.Navigate(new Uri(TargetFileNameTextBox.Text));
                 }
             }
         }
 
 
+
+ 
         public void GetDirectoriesFiles(string path,string targetPath)
         {
             DirectoryInfo d = new DirectoryInfo(path); //Assuming Test is your Folder
-
             var sadsadas = d.GetFileSystemInfos();
 
 
@@ -94,7 +105,21 @@ namespace FileCopyProj
 
         private void CopyButton_Click(object sender, RoutedEventArgs e)
         {
-            GetDirectoriesFiles(SourceFileNameTextBox.Text, TargetFileNameTextBox.Text);
+            if (SourceFileNameTextBox.Text == "" || TargetFileNameTextBox.Text == "" || SourceFileNameTextBox.Text =="Source"|| TargetFileNameTextBox.Text=="Target")
+            {
+                MessageBox.Show("Kaynak ve Hedef Klasör Belirtilmedi", "Softtech");
+            }
+            else if(SourceFileNameTextBox.Text==TargetFileNameTextBox.Text)
+            {
+                MessageBox.Show("Kaynak ve Hedef Klasör Aynı", "Softtech");
+
+            }
+            else
+            {
+                GetDirectoriesFiles(SourceFileNameTextBox.Text, TargetFileNameTextBox.Text);
+                MessageBox.Show("Kopyalama Başarılı", "Softtech");
+
+            }
         }
     }
 }
